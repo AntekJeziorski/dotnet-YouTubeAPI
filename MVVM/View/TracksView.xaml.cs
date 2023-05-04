c463e8fb92346b1ec04eb7db4c3fbb2976e2fe55
Szymon-Sobczak <szymon741852@gmail.com>
2023-05-04 19:14:05 +0200

    Revert "fix database function call2"
    This reverts commit e2dc86ae827dd18f9407ea10cef864757810df58.

--- MVVM/View/TracksView.xaml.cs
+++ MVVM/View/TracksView.xaml.cs
@@ -66,7 +66,7 @@ namespace dotnet_YouTubeAPI.MVVM.View
         {
             using (var context = new YouTubeApiContext())
             {
-                var tracks = context.GetTrackInfo();
+                var tracks = context.getTrackInfo();
                 listView.ItemsSource = tracks;
             }
         }
