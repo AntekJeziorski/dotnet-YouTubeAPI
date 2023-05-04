cb4aecfa8407b4257ced5c518728af4afd75c4d5
AntekJeziorski <antonijeziorski@gmail.com>
2023-05-04 17:54:51 +0200

    add author documentation

--- doc/html/search/classes_0.js
+++ doc/html/search/classes_0.js
@@ -1,9 +1,8 @@
 var searchData=
 [
   ['aboutview_0',['AboutView',['../classdotnet___you_tube_a_p_i_1_1_m_v_v_m_1_1_view_1_1_about_view.html',1,'dotnet_YouTubeAPI::MVVM::View']]],
-  ['app_1',['App',['../class_you_tube_a_p_i_1_1_app.html',1,'YouTubeAPI']]],
-  ['artistview_2',['ArtistView',['../classdotnet___you_tube_a_p_i_1_1_m_v_v_m_1_1_view_1_1_artist_view.html',1,'dotnet_YouTubeAPI::MVVM::View']]],
-  ['author_3',['Author',['../class_you_tube_a_p_i_1_1_author.html',1,'YouTubeAPI']]],
-  ['authorinfo_4',['AuthorInfo',['../class_you_tube_a_p_i_1_1_author_info.html',1,'YouTubeAPI']]],
-  ['authorshistory_5',['AuthorsHistory',['../class_you_tube_a_p_i_1_1_authors_history.html',1,'YouTubeAPI']]]
+  ['artistview_1',['ArtistView',['../classdotnet___you_tube_a_p_i_1_1_m_v_v_m_1_1_view_1_1_artist_view.html',1,'dotnet_YouTubeAPI::MVVM::View']]],
+  ['author_2',['Author',['../class_you_tube_a_p_i_1_1_author.html',1,'YouTubeAPI']]],
+  ['authorinfo_3',['AuthorInfo',['../class_you_tube_a_p_i_1_1_author_info.html',1,'YouTubeAPI']]],
+  ['authorshistory_4',['AuthorsHistory',['../class_you_tube_a_p_i_1_1_authors_history.html',1,'YouTubeAPI']]]
 ];
