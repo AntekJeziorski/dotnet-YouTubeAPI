c5beeeea1d1dd7e47b73d3db3336201354b03d3f
Szymon-Sobczak <szymon741852@gmail.com>
2023-05-05 12:41:23 +0200

    improve ui

--- MVVM/View/MainWindow.xaml.cs
+++ MVVM/View/MainWindow.xaml.cs
@@ -1,19 +1,5 @@
-using dotnet_YouTubeAPI.MVVM.View;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System;
 using System.Timers;
 
 
@@ -36,8 +22,6 @@ namespace YouTubeAPI
             _timer.AutoReset = true;
             // Start the timer.
             _timer.Enabled = true;
-
-      
         }
 
         public static void Update(Object source, ElapsedEventArgs e)
