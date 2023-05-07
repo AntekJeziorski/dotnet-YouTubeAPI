3a282e5d36a25b8c93812517a67ded0e63ffb8c7
AntekJeziorski <antonijeziorski@gmail.com>
2023-05-07 22:59:59 +0200

    rebuild docs

--- docs/html/search/all_12.js
+++ docs/html/search/all_12.js
@@ -5,5 +5,6 @@ var searchData=
   ['youbookie_20_2d_20youtubeapi_20project_2',['YouBookie - YouTubeAPI project',['../md__c___users__acer__documents_dotnet_i_java_dotnet__you_tube_a_p_i__r_e_a_d_m_e.html',1,'']]],
   ['youtubeapi_3',['YouTubeAPI',['../namespace_you_tube_a_p_i.html',1,'']]],
   ['youtubeapicontext_4',['YouTubeApiContext',['../class_you_tube_a_p_i_1_1_you_tube_api_context.html',1,'YouTubeAPI.YouTubeApiContext'],['../class_you_tube_a_p_i_1_1_you_tube_api_context.html#a1bbdb3da2b6feb14332a089e03082db3',1,'YouTubeAPI.YouTubeApiContext.YouTubeApiContext()']]],
-  ['youtubeapidbinitializer_5',['YouTubeApiDbInitializer',['../class_you_tube_a_p_i_1_1_you_tube_api_db_initializer.html',1,'YouTubeAPI']]]
+  ['youtubeapidbinitializer_5',['YouTubeApiDbInitializer',['../class_you_tube_a_p_i_1_1_you_tube_api_db_initializer.html',1,'YouTubeAPI']]],
+  ['youtubeapitests_6',['YouTubeAPITests',['../classdotnet___you_tube_a_p_i_1_1_tests_1_1_you_tube_a_p_i_tests.html',1,'dotnet_YouTubeAPI::Tests']]]
 ];
--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -5,5 +5,8 @@ var searchData=
   ['artistsview_2',['ArtistsView',['../classdotnet___you_tube_a_p_i_1_1_m_v_v_m_1_1_view_1_1_artists_view.html',1,'dotnet_YouTubeAPI::MVVM::View']]],
   ['author_3',['Author',['../class_you_tube_a_p_i_1_1_author.html',1,'YouTubeAPI']]],
   ['authorinfo_4',['AuthorInfo',['../class_you_tube_a_p_i_1_1_author_info.html',1,'YouTubeAPI']]],
-  ['authorshistory_5',['AuthorsHistory',['../class_you_tube_a_p_i_1_1_authors_history.html',1,'YouTubeAPI']]]
+  ['authormock_5',['AuthorMock',['../classdotnet___you_tube_a_p_i_1_1_tests_1_1_author_mock.html',1,'dotnet_YouTubeAPI::Tests']]],
+  ['authorshistory_6',['AuthorsHistory',['../class_you_tube_a_p_i_1_1_authors_history.html',1,'YouTubeAPI']]],
+  ['authorshistorymock_7',['AuthorsHistoryMock',['../classdotnet___you_tube_a_p_i_1_1_tests_1_1_authors_history_mock.html',1,'dotnet_YouTubeAPI::Tests']]],
+  ['authortests_8',['AuthorTests',['../classdotnet___you_tube_a_p_i_1_1_tests_1_1_author_tests.html',1,'dotnet_YouTubeAPI::Tests']]]
 ];
